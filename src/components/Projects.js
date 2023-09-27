05049903c3d8ad6e30efe053cb416e7bdfa838bd
SIVARAMAN ALIAS RAJA S <127581183+Sivaramanraja98@users.noreply.github.com>
2023-09-27 23:07:46 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,55 +1,54 @@
-import React from 'react';
-import './Project.css';
-import { projects } from '../components/Assets/ProjectList';
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 
-const Project = () => {
+const Projects = () => {
+  const [hoveredProject, setHoveredProject] = useState(null);
+
+  const handleHoverEnter = (project) => {
+    setHoveredProject(project);
+  };
+
+  const handleHoverLeave = () => {
+    setHoveredProject(null);
+  };
+
   return (
-<div className='p-5 bg-light' id="project">
-    <div className='Container border'>
-      <div className='Wrapper'>
-        <div className='Title'>Projects</div>
-        <div className='SkillsContainer'>
-          {
-            projects.map((project)=>(
-              
-              <div className='Skill'>
-              <h2 className='SkillTitle'>{project.name}</h2>
-              <img src={project.img} 
-                alt='screenshot' 
-                style={{width:"100%",height:"200px"}}
-              />
-              <div className='SkillList justify-content-center'>
-                <p className='Desc'>{project.description}</p>
-                <a style={{width:"90%"}}
-                  href={project.applink} 
-                  className='link' 
-                  target='_blank' 
-                  rel="noreferrer" >
-                    View App
-                </a>
-                <a style={{width:"90%"}}
-                  href={project.frontendcode} 
-                  className='link'
-                  target='_blank'
-                  rel="noreferrer" >
-                    Front-end Source Code
-                </a>
-                <a style={{width:"90%"}}
-                  href={project.backendcode}  
-                  className='link' 
-                  target='_blank' 
-                  rel="noreferrer" >
-                    Back-end Source Code
-                </a>
-              </div>
-                </div>
-            ))
-          }
-        </div>
-      </div>
-    </div>
-    </div>
-  )
-}
+    <Container fluid>
+      <Row>
+        {projects.map((project, index) => (
+          <Col key={index} md={4}>
+            <Card
+              className="h-100"
+              onMouseEnter={() => handleHoverEnter(project)}
+              onMouseLeave={handleHoverLeave}
+            >
+              <Image src={project.img} alt={project.name} />
+              <Card.Overlay
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                  opacity: hoveredProject === project ? 1 : 0,
+                  transition: 'opacity 0.2s ease-in-out',
+                }}
+              >
+                <Card.Title className="text-white">
+                  {project.name}
+                </Card.Title>
+                <Card.Text className="text-white">
+                  {project.description}
+                </Card.Text>
+              </Card.Overlay>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
-export default Project;
+export default Projects;
+    
