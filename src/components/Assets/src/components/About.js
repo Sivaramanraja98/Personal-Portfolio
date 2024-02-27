607ab3414661bb59d9cb433279fbd99692212f2a
SIVARAMAN ALIAS RAJA S <127581183+Sivaramanraja98@users.noreply.github.com>
2024-02-27 19:09:43 +0530

    Add files via upload

---
+++ src/components/Assets/src/components/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const About = () => {
+  return (
+    <div className="p-5" id="about">
+      <Container className="borde1 align-items-center">
+        <div className="Title">About Me</div>
+        <Row className="justify-content-center">
+          <Col md={10}>
+            <p className="cont">
+              I am a fresh and enthusiastic MERN Stack Developer, I bring a
+              combination of self-driven learning, a positive mindset, and a
+              quick learner's attitude to every project I tackle. My journey in
+              web development has been marked by a strong determination to
+              continuously expand my knowledge and skills. I thrive on the
+              challenges that coding presents, and I approach each task with an
+              insatiable curiosity to learn and grow. This self-motivated
+              approach to learning ensures that I stay up-to-date with the
+              latest technologies and best practices, making me a versatile and
+              adaptable developer ready to take on new and exciting
+              opportunities in the world of web development.
+            </p>
+          </Col>
+        </Row>
+        <Row
+          className="justify-content-centerg-20"
+          style={{ margin: "15px", fontSize: "20px" }}
+        >
+          <Col style={{ textAlign: "center", color: "cornsilk" }}>
+            <ul style={{listStyle:'none'}}>
+              <li>DOB : 02nd Febraury 1998</li>
+              <li>Education : B.Tech - Mechanical Engineering</li>
+              <li>College : Karunya Institute of Technology And Sciences, CBE </li>
+              <li>Batch : 2015 - 2019</li>
+              <li>Location : Tenkai, Tamilnadu</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Assets/src/components/Assets/ProjectList.js
@@ -0,0 +1,63 @@
+import imgchat from "./chat.PNG";
+import imggmail from "./gmail.PNG";
+import imgcrm from "./CRM.PNG";
+import imgauth from "./auth.PNG";
+import imgTODO from "./TodoMERN.PNG"
+
+export let projects = [
+  {
+    id: 1,
+    name: "CRM APPLICATION",
+    description:
+      "This full-stack application lets you store information about customers to help you track the status of every customer relationshipand this will store and edit customer details, as well as keep notes about them",
+    frontendcode: "https://github.com/Sivaramanraja98/WEBCODE-CRM-FRONTEND.git",
+    backendcode: "https://github.com/Sivaramanraja98/WEBCODE-CRM-BACKEND.git",
+    applink: "https://amazing-mousse-4d5cd0.netlify.app/",
+    img: imgcrm,
+  },
+  {
+    id: 2,
+    name: "Gmail Clone",
+    description:
+      "Gmail Clone - Authenticated User can send Email and Read Email . User can send, receive & reply mails between them. Multi user communication & Profile picture updation.",
+    frontendcode: "https://github.com/Sivaramanraja98/Gmail-Clone-Frontend.git",
+    backendcode: "https://github.com/Sivaramanraja98/Gmail-Clone-Backend.git",
+    applink: "https://gmailcloneproject.netlify.app/",
+    img: imggmail,
+  },
+  {
+    id: 3,
+    name: "Reset PassWord with auth",
+    description:
+      "This Application allows the user to register with mail verification, login, reset password with mail verification",
+    frontendcode:
+      "https://github.com/Sivaramanraja98/Passwordreset-frontend.git",
+    backendcode: "https://github.com/Sivaramanraja98/Passwordreset-Backend.git",
+    applink: "https://myauthapp.netlify.app",
+    img: imgauth,
+  },
+  {
+    id: 4,
+    name: "Chat Application",
+    description:
+      "Application that allows users to communicate with each other in real-time via chat messages. Users must sign up and log in to use the app and can send private messages to other users and can see the online status of other users and Users receive notifications when they receive new messages",
+    frontendcode:
+      "https://github.com/Sivaramanraja98/chat-application-frontend.git",
+    backendcode:
+      "https://github.com/Sivaramanraja98/Chat-Application-Backend.git",
+    applink: "https://chatappmern2298.netlify.app",
+    img: imgchat,
+  },
+  {
+    id: 5,
+    name: "ToDo Application",
+    description:
+      "The Todo Application Backend is a server-side implementation designed to support the functionality of a todo application. It provides the necessary APIs and business logic to manage tasks, user authentication, and user data storage.",
+    frontendcode:
+      "https://github.com/Sivaramanraja98/Mern-Todo-Frontend.git",
+    backendcode:
+      "https://github.com/Sivaramanraja98/MERN-TODO-BACKEND.git",
+    applink: "https://merntodo2298.netlify.app/",
+    img: imgTODO,
+  },
+];
